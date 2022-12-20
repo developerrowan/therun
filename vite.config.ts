f5c4b17d4d1e0c7192c50669a13ee9118e209540
Devin Rowan <its@autumnrowan.com>
2022-12-20 00:53:28 -0600

    Change package name due to npm issue

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 	build: {
 		lib: {
 			entry: resolve(__dirname, 'src/index.ts'),
-			name: 'therun',
+			name: 'therungg',
 			fileName: 'index',
 		},
 		minify: 'esbuild',
